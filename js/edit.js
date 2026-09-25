0d53cc638015886b8cf2e6c93f725a18bdaa2ef1
Doilob <108936037+Doilob@users.noreply.github.com>
2026-09-26 01:00:36 +0900

    Update edit.js

--- js/edit.js
+++ js/edit.js
@@ -1,6 +1,6 @@
 // 🔑 GitHub Gist 설정값
 const GIST_ID = "d584cff9f66dc32942cef6c3389befd2";
-const GITHUB_TOKEN = "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"; // ghp_...
+const GITHUB_TOKEN = "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"; // ghp_...
 
 document.addEventListener('DOMContentLoaded', async () => {
   // URL 쿼리 스트링에서 project id 추출 (?id=proj-123)
